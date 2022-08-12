fc13cf944ad969c41bcbd46a4d4fd542e12cbf26
Alanforlan4 <alangb444@gmail.com>
2022-08-12 14:58:21 -0300

    COnfigurando Heroku

--- clients_management/settings.py
+++ clients_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [clients-management4.herokuapp.com]
+ALLOWED_HOSTS = ['clients-management4.herokuapp.com']
 
 
 # Application definition
